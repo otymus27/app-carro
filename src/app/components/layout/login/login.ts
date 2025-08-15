8858ffeeb8372a4d8483dc93398f6cc6ebde1633
Fábio de Alencar Rocha <otymus27@gmail.com>
2025-08-14 21:10:49 -0300

    Componente menu implementado

--- src/app/components/layout/login/login.ts
+++ src/app/components/layout/login/login.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { MdbFormsModule} from 'mdb-angular-ui-kit/forms';
+import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [MdbFormsModule],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
+  usuario!:string;
+  senha!: string;
+
+  //aqui é uma injeção de dependencia parecido o autowired do java
+  router = inject(Router);
+
+  logar(): void {
+    if (this.usuario == 'admin' && this.senha == '123') {
+      //redirecionar para pagina
+      this.router.navigate(['admin/carros']);
+    }else {
+      alert('Usuário ou senha invalidos!');
+    }
+  }
 
 }
