35f8ce272dd87739b572087ee58d1791206b9e7d
Fábio <otymus27@gmail.com>
2025-08-18 16:56:30 -0300

    Implementando paginação

--- src/app/components/marca/marca.component.ts
+++ src/app/components/marca/marca.component.ts
@@ -1,6 +1,10 @@
 import { Component, inject, TemplateRef, ViewChild } from '@angular/core';
 import { MarcaService } from '../../services/marca.service';
-import { MdbModalModule, MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+import {
+  MdbModalModule,
+  MdbModalRef,
+  MdbModalService,
+} from 'mdb-angular-ui-kit/modal';
 import { Marca } from '../../models/marca';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -11,34 +15,40 @@ import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
   imports: [
     MdbModalModule,
     CommonModule,
-    FormsModule,       // ✅ necessário para [(ngModel)]
-    MdbFormsModule     // ✅ necessário para mdbInput
+    FormsModule, // ✅ necessário para [(ngModel)]
+    MdbFormsModule, // ✅ necessário para mdbInput
   ],
   templateUrl: './marca.component.html',
-  styleUrl: './marca.component.scss'
+  styleUrl: './marca.component.scss',
 })
 export class MarcaComponent {
-
   lista: Marca[] = [];
-  
+
+  //Configuração para paginação
+  page = 0;
+  size = 5;
+  totalPages = 0;
+
   marcaSelecionada: Marca = { id: 0, nome: '' };
 
   marcaService = inject(MarcaService);
   modalService = inject(MdbModalService);
-  @ViewChild("modalMarcaDetalhe") modalMarcaDetalhe!: TemplateRef<any>;
+  @ViewChild('modalMarcaDetalhe') modalMarcaDetalhe!: TemplateRef<any>;
   modalRef!: MdbModalRef<any>;
 
-  constructor() { this.listar(); }
+  constructor() {
+    this.listar();
+  }
 
   listar() {
     this.marcaService.listar().subscribe({
-      next: lista => this.lista = lista,
-      error: () => alert('Erro ao listar marcas!')
+      next: (lista) => (this.lista = lista),
+      error: () => alert('Erro ao listar marcas!'),
     });
   }
 
   CadastrarModal() {
-    this.marcaSelecionada = { id:0, nome: '' };
+    this.marcaSelecionada = { id: 0, nome: '' };
     this.modalRef = this.modalService.open(this.modalMarcaDetalhe);
   }
 
@@ -51,22 +61,22 @@ export class MarcaComponent {
     this.modalRef.close();
   }
 
-  salvarMarca(marca: Marca) {   
+  salvarMarca(marca: Marca) {
     const isNovaMarca = !marca.id || marca.id <= 0;
 
     if (isNovaMarca) {
       // Cria registro sem id
       const novaMarca: Partial<Marca> = { nome: marca.nome };
       this.marcaService.save(novaMarca).subscribe({
-        next: (res: Marca) => {        
+        next: (res: Marca) => {
           alert(`Registro cadastrada com sucesso!`);
           this.listar(); // Atualiza a lista após cadastro
           this.modalRef?.close(); // aqui fecha o modal
         },
         error: (err) => {
-          console.error("Erro ao salvar registro:", err);
-          alert("Erro ao salvar registro");
-        }
+          console.error('Erro ao salvar registro:', err);
+          alert('Erro ao salvar registro');
+        },
       });
     } else {
       // Atualiza marca existente - envia Marca completo
@@ -79,28 +89,24 @@ export class MarcaComponent {
         error: (err) => {
           const erroMsg = err?.error || 'Erro desconhecido!';
           alert(`Erro ${err.status || ''}: ${erroMsg}`);
-        }
+        },
       });
     }
   }
 
-
-
   excluir(marca: Marca) {
     if (!confirm(`Deseja excluir ${marca.nome}?`)) return;
     this.marcaService.excluir(marca.id!).subscribe({
-      next: mensagem => { // quando o backend retornar o que se espera
-          mensagem = mensagem || 'Registro excluído com sucesso!';
-          alert(mensagem);
-          this.listar(); // Atualiza a lista após exclusão
-        },
-        error: error => { // quando ocorrer qualquer erro de comunicação com o backend
-          alert('Algo errado no serviço excluir!')
-        }
+      next: (mensagem) => {
+        // quando o backend retornar o que se espera
+        mensagem = mensagem || 'Registro excluído com sucesso!';
+        alert(mensagem);
+        this.listar(); // Atualiza a lista após exclusão
+      },
+      error: (error) => {
+        // quando ocorrer qualquer erro de comunicação com o backend
+        alert('Algo errado no serviço excluir!');
+      },
     });
   }
-
-  
-  
-
 }
--- src/app/services/marca.service.ts
+++ src/app/services/marca.service.ts
@@ -1,6 +1,6 @@
-import {inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Marca } from '../models/marca';
 
 export interface ResponseMsg {
@@ -8,36 +8,44 @@ export interface ResponseMsg {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MarcaService {
-
   http = inject(HttpClient);
 
   API = 'http://localhost:8081/api/marca';
- 
 
-  constructor() { }
+  constructor() {}
 
-  listar(): Observable<Marca[]>{
-    return this.http.get<Marca[]>(this.API );
+  listar(): Observable<Marca[]> {
+    return this.http.get<Marca[]>(this.API);
   }
 
-  excluir(id:number): Observable<string> {
-    return this.http.delete<string>(this.API + "/"+id, {responseType: 'text' as 'json'});
+  listarPaginado(page: number, size: number): Observable<any> {
+    let params = new HttpParams().set('page', page).set('size', size);
+
+    return this.http.get<any>(this.API, { params });
   }
- 
+
+  excluir(id: number): Observable<string> {
+    return this.http.delete<string>(this.API + '/' + id, {
+      responseType: 'text' as 'json',
+    });
+  }
 
   save(marca: Partial<Marca>): Observable<Marca> {
-    return this.http.post<Marca>(this.API, marca, {responseType: 'text' as 'json'});
+    return this.http.post<Marca>(this.API, marca, {
+      responseType: 'text' as 'json',
+    });
   }
 
-  update(marca: Marca, id:number): Observable<string> {
-    return this.http.put<string>(this.API + "/"+id, marca, {responseType: 'text' as 'json'});
+  update(marca: Marca, id: number): Observable<string> {
+    return this.http.put<string>(this.API + '/' + id, marca, {
+      responseType: 'text' as 'json',
+    });
   }
-  
 
   buscarPorId(id: number): Observable<Marca> {
-    return this.http.get<Marca>(this.API + "/"+id);
+    return this.http.get<Marca>(this.API + '/' + id);
   }
 }
