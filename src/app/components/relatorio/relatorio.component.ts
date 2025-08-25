2929241d93c2d28d5b92b02728f3240af23ce76e
Fábio <otymus27@gmail.com>
2025-08-25 15:56:28 -0300

    Funcionalidade relatorio - v1

---
+++ src/app/components/relatorio/relatorio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relatorio',
+  imports: [],
+  templateUrl: './relatorio.component.html',
+  styleUrl: './relatorio.component.scss'
+})
+export class RelatorioComponent {
+
+}
